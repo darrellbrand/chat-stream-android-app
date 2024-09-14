0fe367030c8df0a680f4bca79560445d61a15b0e
dj <darrellbrand@gmail.com>
2024-09-14 00:08:02 -0700

    rad almost done

---
+++ app/src/main/java/com/djf/chatclient/AvatarUtil.kt
@@ -0,0 +1,10 @@
+package com.djf.chatclient
+
+val avatarList = listOf(
+    "https://cdn.iconscout.com/icon/free/png-512/free-avatar-icon-download-in-svg-png-gif-file-formats--user-man-avatars-flat-icons-pack-people-456323.png?f=webp&w=256",
+    "https://cdn.iconscout.com/icon/free/png-512/free-avatar-icon-download-in-svg-png-gif-file-formats--user-devil-avatars-flat-icons-pack-people-456326.png?f=webp&w=256",
+    "https://cdn.iconscout.com/icon/premium/png-512-thumb/avatar-36-116394.png?f=webp&w=256",
+    "https://cdn.iconscout.com/icon/premium/png-512-thumb/avatar-39-116397.png?f=webp&w=256",
+    "https://cdn.iconscout.com/icon/premium/png-512-thumb/avatar-37-116395.png?f=webp&w=256",
+    "https://cdn.iconscout.com/icon/premium/png-512-thumb/avatar-93-116459.png?f=webp&w=256"
+)
--- app/src/main/java/com/djf/chatclient/presentation/screens/LoginScreen.kt
+++ app/src/main/java/com/djf/chatclient/presentation/screens/LoginScreen.kt
@@ -1,9 +1,15 @@
 package com.djf.chatclient.presentation.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +18,7 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -25,16 +32,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.djf.chatclient.R
+import com.djf.chatclient.avatarList
 
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun LoginScreen(onClick: (name: String, image: String) -> Unit) {
     var name by rememberSaveable { mutableStateOf("name") }
-    var image by rememberSaveable { mutableStateOf("") }
+    var image by rememberSaveable { mutableStateOf("https://cdn.iconscout.com/icon/free/png-512/free-avatar-icon-download-in-svg-png-gif-file-formats--user-man-avatars-flat-icons-pack-people-456323.png?f=webp&w=256") }
     Box {
         Image(
             painter = painterResource(R.drawable.large_triangles),
@@ -66,21 +77,39 @@ fun LoginScreen(onClick: (name: String, image: String) -> Unit) {
                     )
                     .fillMaxWidth()
             )
-            Spacer(modifier = Modifier.height(20.dp))
-            TextField(
-                image,
-                { image = it },
-                label = { Text("Image Url") },
-                modifier = Modifier
-                    .padding(5.dp)
-                    .clip(RoundedCornerShape(15.dp))
-                    .fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(50.dp))
+            Spacer(modifier = Modifier.height(25.dp))
             Button(onClick = {
                 onClick(name, image)
-            }) {
+            }, modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp)) {
                 Text("Login")
+            }
+            Spacer(modifier = Modifier.height(25.dp))
+            FlowRow {
+                avatarList.forEach {
+                    Box(
+                        modifier = Modifier
+                            .clickable { image = it }
+                            .padding(10.dp)
+                            .clip(CircleShape)
+                            .border(
+                                width = 7.dp, color =
+                                if (it == image) {
+                                    Color.Blue
+                                } else {
+                                    Color.Transparent
+                                }, shape = CircleShape
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        AsyncImage(
+                            model = it,
+                            contentDescription = "",
+                            contentScale = ContentScale.Crop,
+                        )
+                    }
+                }
             }
         }
     }
